448fa84923b7293c3f9cbe24406e28b7c611e017
kazitishan <kazitishan12@gmail.com>
2025-06-25 00:31:28 -0400

    Update the navigation bar so that you can easily navigate to other competitions

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,11 +1,52 @@
 import Link from "next/link";
 
-function NavBar(){
-    return(
-        <nav className="px-8 flex justify-center items-center bg-[#284e13ff]">
-            <Link href="/" className="">
-                <img src="/logo.svg" alt="Kazino Sportsbook Logo" className="w-30 h-25" />
-            </Link>
+function NavBar() {
+    const competitions = [
+        "UEFA Nations League",
+        "Copa America",
+        "UCL",
+        "UEL",
+        "UECL",
+        "UEFA Super Cup",
+        "FIFA Club World Cup",
+        "Premier League",
+        "La Liga",
+        "Bundesliga",
+        "Serie A",
+        "Ligue 1",
+        "EFL Championship",
+        "FA Cup",
+        "EFL Cup",
+        "Community Shield",
+        "Supercopa de España",
+        "Copa del Rey",
+        "DFB Pokal",
+        "German Super Cup",
+        "Coppa Italia",
+        "Supercoppa Italiana",
+        "Coupe de France",
+        "French Super Cup"
+    ];
+
+    return (
+        <nav className="bg-white shadow-md">
+            <div className="px-8 flex justify-center items-center bg-[#284e13ff]">
+                <Link href="/" className="">
+                    <img src="/logo.svg" alt="Kazino Sportsbook Logo" className="w-30 h-25" />
+                </Link>
+            </div>
+            
+            <div className="flex flex-wrap justify-center gap-2 p-4 bg-gray-100">
+                {competitions.map((comp, index) => (
+                    <Link 
+                        href={`/${comp}`} 
+                        key={`${comp}-${index}`} 
+                        className="bg-blue-100 hover:bg-blue-200 text-blue-800 text-sm font-medium px-3 py-1 rounded-full whitespace-nowrap transition-colors"
+                    >
+                        {comp}
+                    </Link>
+                ))}
+            </div>
         </nav>
     );
 }
